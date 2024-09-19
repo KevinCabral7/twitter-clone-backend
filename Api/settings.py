50036ae4331d69fae711345f36926ac8a9373111
Kevin Cabral <kevin1tdsb@gmail.com>
2024-09-19 15:32:58 -0300

    change csrf trusted origins

--- Api/settings.py
+++ Api/settings.py
@@ -137,13 +137,16 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ['http://twitter-clone-frontend-production-99d1.up.railway.app', "http://localhost:3000"]
+CSRF_TRUSTED_ORIGINS = ['https://twitter-clone-frontend-production-99d1.up.railway.app', "http://localhost:3000"]
 
 SIMPLE_JWT = {
     "ACESS_TOKEN_LIFETIME":  timedelta(minutes=30),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1)
 }
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3030',
+    'https://twitter-clone-frontend-production-99d1.up.railway.app'
+]
 CORS_ALLOW_CREDENTIALS = True
 
 
