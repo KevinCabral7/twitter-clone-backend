5371bf4766da3ab827d9d18de792d443c0372df1
Kevin Cabral <kevin1tdsb@gmail.com>
2024-09-24 11:36:37 -0300

    change entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@ echo 'Applying migrations...'
 python manage.py migrate
 
 echo 'Running server...'
-uvicorn Api.asgi:application --bind 0.0.0.0:$PORT
+uvicorn Api.asgi:application --host 0.0.0.0 --port $PORT
