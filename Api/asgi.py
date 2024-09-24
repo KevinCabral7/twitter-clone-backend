b0dec6b2ea19001e3fce9251d9861eee51176cf5
Kevin Cabral <kevin1tdsb@gmail.com>
2024-09-24 12:04:38 -0300

    requirements.txt

--- Api/asgi.py
+++ Api/asgi.py
@@ -12,9 +12,8 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Api.settings')
 import twitter.routing 
-os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Api.settings')
 application = ProtocolTypeRouter(
     {
         'http': get_asgi_application(),
