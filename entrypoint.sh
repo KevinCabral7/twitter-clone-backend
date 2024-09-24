64065f6917dc1666ba99503918e7c2e58498c277
Kevin Cabral <kevin1tdsb@gmail.com>
2024-09-23 22:58:32 -0300

    change entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@ echo 'Applying migrations...'
 python manage.py migrate
 
 echo 'Running server...'
-gunicorn Api.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn Api.asgi:application --bind 0.0.0.0:$PORT
