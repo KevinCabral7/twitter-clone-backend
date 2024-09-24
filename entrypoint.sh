07e66afff6c954f20dd59eeba83e8e74d4d9d125
Kevin Cabral <kevin1tdsb@gmail.com>
2024-09-24 12:38:06 -0300

    entrypoint changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,5 @@ export DJANGO_SETTINGS_MODULE=Api.settings
 echo 'Applying migrations...'
 python manage.py migrate
 
-echo 'Catching static files...'
-python manage.py collectstatic --noinput
-
 echo 'Running server...'
 daphne -b 0.0.0.0 -p $PORT Api.asgi:application
