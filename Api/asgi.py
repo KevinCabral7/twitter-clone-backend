654668dbe5249545d9ebb64deee15be053fc9bf7
Kevin Cabral <kevin1tdsb@gmail.com>
2024-09-24 11:10:26 -0300

    change entrypoint.sh

--- Api/asgi.py
+++ Api/asgi.py
@@ -15,7 +15,6 @@ from django.core.asgi import get_asgi_application
 import twitter.routing 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Api.settings')
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
-
 application = ProtocolTypeRouter(
     {
         'http': get_asgi_application(),
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@ echo 'Applying migrations...'
 python manage.py migrate
 
 echo 'Running server...'
-gunicorn Api.asgi:application --bind 0.0.0.0:$PORT
+daphne Api.asgi:application --bind 0.0.0.0:$PORT
