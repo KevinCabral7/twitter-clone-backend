7d40f22cf0e0aa807675ee7c27df8f42ede48df5
Kevin Cabral <kevin1tdsb@gmail.com>
2024-09-24 13:15:48 -0300

    entrypoint changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ echo 'Applying migrations...'
 python manage.py migrate
 
 echo 'Running server...'
-daphne -b 0.0.0.0 -p $PORT Api.asgi:application
+gunicorn -b 0.0.0.0 -p $PORT Api.asgi:application
