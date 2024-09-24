4818dd972881be7d53a71f3f3f27a9bae2a0d34c
Kevin Cabral <kevin1tdsb@gmail.com>
2024-09-24 12:42:26 -0300

    entrypoint changes

--- Api/asgi.py
+++ Api/asgi.py
@@ -8,13 +8,16 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Api.settings')
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 import twitter.routing 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Api.settings')
+
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
+
 application = ProtocolTypeRouter(
     {
         'http': get_asgi_application(),
